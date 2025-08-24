ca0341edef6746e98bd1da4bbda873406bb3f4d6
Alan Quiroga <141192566+Alan-oss55@users.noreply.github.com>
2025-08-24 03:05:57 -0300

    logica login

--- gestor_tareas/login.php
+++ gestor_tareas/login.php
@@ -1,3 +1,43 @@
+<?php 
+
+    session_start() ;
+
+    require("connection.php") ;
+
+    if ( $_SERVER["REQUEST_METHOD"] == "POST" ){
+
+        $username = $_POST["username"] ;
+        $password = $_POST["password"] ;
+
+        $sent_base = "SELECT * FROM usuarios WHERE username = ? " ;
+
+        $stmt = $conexion_bd->prepare($sent_base) ;
+
+        $stmt->bind_param("s", $username) ;
+
+        $stmt->execute() ;
+
+        $resultado = $stmt->get_result() ;
+
+        $usuario = $resultado->fetch_assoc() ;
+
+        $stmt->close() ;
+
+        if ( $usuario && password_verify($password, $usuario["password"] ) ) {
+
+            $_SESSION["id_usuario"] = $usuario["id"] ;
+            header("Location: inicio.php") ;
+            exit;
+        }else{
+            echo "Usuario o contraseña incorrectos" ;
+        }
+    }
+
+
+?>
+
+
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -9,7 +49,4 @@
         
     </body>
 
-    <?php 
-    
-    ?>
 </html>
